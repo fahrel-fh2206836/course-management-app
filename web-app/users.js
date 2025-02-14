24ea7ed94c67e28430e2cf1efafd292c1c920601
Ghanim Alkuwari <ga2208523@qu.edu.qa>
2025-02-14 04:12:00 +0300

    json and javascript
    was informed by fahrel to added the functionality of the login webpage, i made a json file to format and save pre existing usernames/passwords. also made the JavaScript that fetches the json file and adds responses and improves overall security of the webpage. also creates objects that can be saved and makes sure the credentials are valid

---
+++ web-app/users.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('LoginForm');
+
+    if (!loginForm) {
+        console.error("error");
+        return;
+    }
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+
+fetch('login.json')
+    .then(response => response.json())
+    .then(users => {
+        const user = users.find(user => user.username === username && user.password === password);
+        if (user) {
+            window.location.href = 'dashboard.html';
+        } else { 
+            alert('credentials are incorrect or doesnt exist, retry please sir')
+        }
+    })
+
+    .catch(error => {
+        console.error('user error', error);
+    });
+
+})})
