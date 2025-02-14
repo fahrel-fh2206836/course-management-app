4695b82ceccb28309ae0638d54235c57d8857647
Fahrel <fahrelazki@gmail.com>
2025-02-14 18:33:28 +0300

    fixed nav bar errors

--- web-app/scripts/navbar-script.js
+++ web-app/scripts/navbar-script.js
@@ -1,26 +1,35 @@
 let menuList = document.getElementById("menuList")
 let navbar1 = document.getElementById("navbar1")
 
-function toggleMenu() {
-    if(menuList.style.maxHeight == "0px") {
-        menuList.style.maxHeight = "300px"
-        navbar1.style.borderRadius = "0px"
-        menuList.style.boxShadow = "0 0 10px"
-        menuList.style.padding = "20px"
-    }else {
-        menuList.style.maxHeight = "0px"
-        navbar1.style.borderRadius = null
-        menuList.style.boxShadow = "none"
-        menuList.style.padding = null
-    }
+if(window.innerWidth > 1024) {
+    menuList.classList.add("largeMenuList")
+} else {
+    menuList.classList.add("openMenuList")
+    menuList.classList.add("closeMenuList")
 }
 
-$(window).on('resize load', function () {
-    if ($(window).width() < 739) {
-        $("#default").add('classnew').removeClass('classold')
+function toggleMenu() {
+    if(menuList.classList.contains("closeMenuList")) {
+        navbar1.style.borderRadius = "0px"
+        menuList.classList.remove("closeMenuList")
+    }else {
+        navbar1.style.borderRadius = null 
+        menuList.classList.add("closeMenuList")
+
+   }
+}
+
+window.addEventListener('resize', function(event) {
+    if(this.window.innerWidth > 1024) {
+        menuList.classList.remove("openMenuList")
+        menuList.classList.remove("closeMenuList")
+        navbar1.style.borderRadius = null
+        menuList.classList.add("largeMenuList")
     } else {
-        $("#default").addClass('classold').removeClass('classnew')
+        menuList.classList.remove("largeMenuList")
+        menuList.classList.add("openMenuList")
+        menuList.classList.add("closeMenuList")
     }
- });
+}, true);
 
 
