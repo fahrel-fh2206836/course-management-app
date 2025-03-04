9752b823da3a3c39bd293a96ceea5ffe45e37bf7
Fahrel <fahrelazki@gmail.com>
2025-03-04 23:07:14 +0300

    added logout dialog

---
+++ web-app/js/navbar-script.js
@@ -0,0 +1,34 @@
+let menuList = document.querySelector("#menuList");
+let navbar = document.querySelector("#navbar");
+
+if(window.innerWidth > 1024) {
+    menuList.classList.add("largeMenuList");
+} else {
+    menuList.classList.add("closeMenuList");
+}
+
+window.addEventListener('resize', function(event) {
+    if(this.window.innerWidth > 1024) {
+        menuList.classList.remove("openMenuList");
+        menuList.classList.remove("closeMenuList");
+        navbar.style.borderRadius = null;
+        menuList.classList.add("largeMenuList");
+    } else {
+        menuList.classList.remove("largeMenuList");
+        menuList.classList.add("closeMenuList");
+    }
+    }, true);
+
+
+function toggleMenu() {
+    if(!menuList.classList.contains("openMenuList")) {
+        menuList.classList.add("openMenuList");
+    }
+    if(menuList.classList.contains("closeMenuList")) {
+        navbar.style.borderRadius = "0px";
+        menuList.classList.remove("closeMenuList");
+    }else {
+        navbar.style.borderRadius = null;
+        menuList.classList.add("closeMenuList");
+    }
+}    
