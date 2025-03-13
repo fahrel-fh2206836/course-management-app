691e715749ee171f856e7ee11d0838419a67d9b6
Fahrel <fahrelazki@gmail.com>
2025-03-13 16:16:43 +0300

    installed necessary packages fs-extra and nanoid

--- web-app/repo/app-repo.js
+++ web-app/repo/app-repo.js
@@ -1,6 +1,7 @@
-import fse from 'fs-extra';
+// In Lab 8, We learnt this if you guys are wondering.
+import fse from 'fs-extra'; // For reading and writing JSON files
 import path from 'path';
-import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid'; // Generates a random String ID
 
 class AppRepo {
 
@@ -8,4 +9,4 @@ class AppRepo {
 
 export default new AppRepo();
 
-console.log("hi");
+console.log(nanoid());
