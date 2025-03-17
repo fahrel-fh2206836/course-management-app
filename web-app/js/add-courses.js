f4d6ee229dfb832101e41c502481945fb9c6b5b0
Fahrel <fahrelazki@gmail.com>
2025-03-17 09:34:52 +0300

    added functionality for add courses

---
+++ web-app/js/add-courses.js
@@ -0,0 +1,37 @@
+//Local Storages
+const courses = JSON.parse(localStorage.courses);
+
+//Inputs
+const form = document.querySelector("#add-courses-form");
+const courseCode = document.querySelector("#course-code");
+const courseName = document.querySelector("#course-name");
+const creditHour = document.querySelector("#credit-hour");
+const majorDropdown = document.querySelector("#major");
+const prerequisitesDropdown = document.querySelector("prerequisites");
+const ongoingCheckbox = document.querySelector("ongoing");
+const regCheckbox = document.querySelector("registration");
+
+//Initialize prerequisitesDropdown values
+const csCoursesOpt = document.querySelector("#cs-courses");
+const ceCoursesOpt = document.querySelector("#ce-courses");
+
+renderCourses();
+
+function renderCourses() {
+    const csCourses = courses.filter(c => c.majorId === "1");
+    const ceCourses = courses.filter(c => c.majorId === "2");
+    
+    csCoursesOpt.innerHTML = csCourses.map(c => courseOptionsToHTML(c)).join('\n');
+    ceCoursesOpt.innerHTML = ceCourses.map(c => courseOptionsToHTML(c)).join('\n');
+}   
+
+function courseOptionsToHTML(c) {
+    return `<option label="${c.courseName}" value="${c.id}"></option>`;
+}
+
+
+form.addEventListener('submit', handleAddCourse);
+
+function handleAddCourse() {
+
+}
