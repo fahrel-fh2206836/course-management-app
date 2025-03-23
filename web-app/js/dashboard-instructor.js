d79c2b8abf7ec08c755c276a10c52ae7abf9ed0f
aw2211598 <mohammedwasay45@gmail.com>
2025-03-23 19:15:10 +0300

    updated a few things

---
+++ web-app/js/dashboard-instructor.js
@@ -0,0 +1,6 @@
+const user = JSON.parse(localStorage.getItem("loggedInUser"));
+const majors = JSON.parse(localStorage.majors);
+const registrations = JSON.parse(localStorage.registrations);
+const sections = JSON.parse(localStorage.sections);
+const courses = JSON.parse(localStorage.courses);
+const users = JSON.parse(localStorage.users);
