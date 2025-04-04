7212aaa7378dd8221dd90696641ab746e42b8a99
aw2211598 <mohammedwasay45@gmail.com>
2025-04-05 01:48:52 +0300

    made a few changes to styling,html and js

--- web-app/js/dashboard-instructor.js
+++ web-app/js/dashboard-instructor.js
@@ -6,30 +6,49 @@ const courses = JSON.parse(localStorage.courses);
 const users = JSON.parse(localStorage.users);
 
 const nameSpan = document.querySelector("#name");
+const numberOfClasses = document.querySelector("#NoClasses")
 const teachingList = document.querySelector(".course-card-list");
+const instructorSections = sections.filter(section => section.instructorId === user.userId);
 
 nameSpan.innerText = `${user.firstName} ${user.lastName}`;
-const instructorSections = sections.filter(section => section.instructorId === user.userId);
+
 
 
 function renderActiveCourses(){
-    let html = ``;
+    let ongoingHTML = '';
+    let futureHTML = '';
+    let pastHTML = '';
     if(instructorSections.length === 0) {
         html = `<div class="empty-section">
                                     <i class='bx bxs-error-circle'></i>
-                                    <p>No Active Courses.</p>
+                                    <p>No Courses found.</p>
                                  </div>
                                 `;
-    } else {
-        instructorSections.forEach(instSec => {
-            let section = sections.find(s => instSec.sectionId === s.sectionId); 
-            let course = courses.find(c => c.id === section.courseId);
-            let NoStud = registrations.filter(r => r.sectionId === section.sectionId).length;;
-            html+=generateCourseListHTML(section, course, NoStud)+'\n';
-        })
+                                return;
     }
-    console.log(html); 
-    teachingList.innerHTML = html;
+    instructorSections.forEach(instSec => {
+        let section = sections.find(s => instSec.sectionId === s.sectionId); 
+        let course = courses.find(c => c.id === section.courseId);
+        let NoStud = registrations.filter(r => r.sectionId === section.sectionId).length;;
+        const cardHTML = generateCourseListHTML(section, course, NoStud)+'\n';
+        if (section.sectionStatus === 'ONGOING') {
+            ongoingHTML += cardHTML;
+          } else if (section.sectionStatus === 'OPEN_REGISTRATION') {
+            futureHTML += cardHTML;
+          } else if (section.sectionStatus === 'COMPLETED') {
+            pastHTML += cardHTML;
+          }
+        })
+        teachingList.innerHTML = `
+            <h3>Ongoing Courses</h3>
+            <div class="card-group">${ongoingHTML || '<p>No ongoing courses.</p>'}</div>
+            
+            <h3>Future Courses</h3>
+            <div class="card-group">${futureHTML || '<p>No upcoming courses.</p>'}</div>
+            
+            <h3>Completed Courses</h3>
+            <div class="card-group">${pastHTML || '<p>No completed courses.</p>'}</div>
+        `;
 }
 
 function generateCourseListHTML(s, c, n) {
@@ -37,7 +56,7 @@ function generateCourseListHTML(s, c, n) {
             <div class="course-card hover-underline" onclick="location.href='grade-allocation.html'">
                 <div class="card-flag"><p>${c.courseCode}</p></div>
                 <div class="card-course-name"><p>${c.courseName}</p></div>
-                <div class="card-course-instructor"><p>Student Number:</p><p>${n}</p></div>
+                <div class="card-course-instructor"><p>Number of students:</p><p>${n}</p></div>
                 <div class="card-course-section-location"><p>Section ID: ${s.sectionId}</p><p>Class Location: ${s.location !== '' ? s.location : 'None'}</p></div>
                 <hr>
 <div class="card-course-sem-schedule"><p><i class='bx bx-calendar'></i>${s.semester}</p><p><i class='bx bx-calendar-week'></i>${s.schedule.days !== '' ? s.schedule.days : 'None'}</p><p><i class='bx bx-time'></i>${s.schedule.time !== '' ? s.schedule.time : 'None'}</p></div>
