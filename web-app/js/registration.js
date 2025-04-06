b99d502fa694acd70d3c27ad32dd4c3a8230d897
Mohammed Mansib Shahriyar Alam <mohdmansib@gmail.com>
2025-04-06 04:18:03 +0300

    course details done

--- web-app/js/registration.js
+++ web-app/js/registration.js
@@ -1,9 +1,17 @@
 const selectedCourse = JSON.parse(localStorage.selectedCourse);
 const title = document.querySelector("#title");
+const courseId = document.querySelector("#course-id");
+const courseCode = document.querySelector("#course-code");
+const courseName = document.querySelector("#course-name");
+const courseHours = document.querySelector("#course-hour");
+const preReqCoursesDisplay = document.querySelector("pre-courses");
 
+const coursePrerequisites = selectedCourse.prerequisitesCoursesId;
+const allCourses = localStorage.courses;
 
-title.innerHTML = headingHtml();
+title.innerHTML = `View Sections for ${selectedCourse.courseName}`;
 
-function headingHtml(){
-    return `View Sections for ${selectedCourse.courseName}`
-}
+courseId.innerHTML = `${selectedCourse.id}`
+courseCode.innerHTML = `${selectedCourse.courseCode}`
+courseName.innerHTML = `${selectedCourse.courseName}`
+courseHours.innerHTML = `${selectedCourse.creditHour}`
