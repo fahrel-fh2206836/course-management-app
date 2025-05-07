73fc57081d444529f7e00a171c333d9a9170ffd5
Fahrel <fahrelazki@gmail.com>
2025-05-07 12:06:20 +0300

    finished instructor dashboard

--- web-app-phase-2/app/api/semester/route.js
+++ web-app-phase-2/app/api/semester/route.js
@@ -1,6 +1,9 @@
 import appRepo from "@/app/repo/app-repo";
 
 export async function GET(req) {
-   const semesters = await appRepo.getSemesters();
+   const { searchParams } = new URL(req.url);
+   let removeSem = searchParams.get('removeSem');
+   const removeSemArray = removeSem ? removeSem.split(',') : [];
+   const semesters = await appRepo.getSemesters(removeSemArray);
    return Response.json(semesters, { status: 200 });
 }
