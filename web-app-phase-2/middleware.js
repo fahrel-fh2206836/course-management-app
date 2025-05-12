78d1c921d2f657b5856465cd362ca12c7f37e183
Fahrel <fahrelazki@gmail.com>
2025-05-12 19:43:30 +0300

    commented middleware

--- web-app-phase-2/middleware.js
+++ web-app-phase-2/middleware.js
@@ -1,35 +1,35 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+// export async function middleware(req) {
+//   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
 
 
-  if (!token || !token.user || new Date(token.exp * 1000) < new Date()) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (!token || !token.user || new Date(token.exp * 1000) < new Date()) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  const role = token.user.role;
-  const url = req.nextUrl;
+//   const role = token.user.role;
+//   const url = req.nextUrl;
 
-  console.log(role,url,token);
+//   console.log(role,url,token);
   
   
-  if (url.pathname.startsWith("/view-admin") && role !== "admin") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (url.pathname.startsWith("/view-admin") && role !== "admin") {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  if (url.pathname.startsWith("/view-student") && role !== "Student") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (url.pathname.startsWith("/view-student") && role !== "Student") {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  if (url.pathname.startsWith("/view-instructor") && role !== "Instructor") {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (url.pathname.startsWith("/view-instructor") && role !== "Instructor") {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
 export const config = {
     matcher: ["/view-admin/:path*", "/view-student/:path*", "/view-instructor/:path*"],
